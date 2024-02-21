795120f38fe82a010d5ec83d46b4ddf61cc053e3
RolletQuentin <rolletquen@gmail.com>
2024-02-21 10:43:20 +0100

    fix some test for Huffman
    Huffman trees are not unique. We make a generic tree adn see if there are equivalent or not.

--- test/StatisticTest/HuffmanTest.hs
+++ test/StatisticTest/HuffmanTest.hs
@@ -6,6 +6,12 @@ module StatisticTest.HuffmanTest (
 import Test.HUnit
 import Statistic.EncodingTree
 import Statistic.Huffman
+import qualified Data.Tree as T
+
+-- To transform a Huffman Tree in Generic Tree
+toGenericTree :: EncodingTree a -> T.Tree (Int, Maybe a)
+toGenericTree (EncodingLeaf cnt ltr) = T.Node (cnt, Just ltr) []
+toGenericTree (EncodingNode cnt lft rgt) = T.Node (cnt, Nothing) [toGenericTree lft, toGenericTree rgt]
 
 -- Test cases for Huffman tree generation
 testHuffmanTree :: Test
@@ -13,5 +19,5 @@ testHuffmanTree = TestList
     [ 
         "Huffman Tree - Empty Input" ~: tree ([] :: [Char]) ~?= Nothing,
         "Huffman Tree - Single Symbol" ~: tree "a" ~?= Just (EncodingLeaf 1 'a'),
-        "Huffman Tree - Multiple Symbols" ~: tree "aabcc" ~?= Just (EncodingNode 5 (EncodingNode 2 (EncodingLeaf 1 'b') (EncodingLeaf 1 'c')) (EncodingLeaf 2 'a'))
+        "Huffman Tree - Multiple Symbols" ~: toGenericTree <$> tree "aabcc" ~?= Just (toGenericTree (EncodingNode 5 (EncodingLeaf 2 'c') (EncodingNode 3 (EncodingLeaf 1 'b') (EncodingLeaf 2 'a'))))
     ]
