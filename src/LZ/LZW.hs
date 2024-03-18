50168ecde4f590e81aebeac67cedf2e1bcd38df0
SOULIER Patrice <p.patrice976@hotmail.com>
2024-03-18 20:39:00 +0100

    fixing some errors in LZW.hs compress function

--- src/LZ/LZW.hs
+++ src/LZ/LZW.hs
@@ -9,9 +9,9 @@ module LZ.LZW(compress, uncompress) where
 import LZ.Dictionaries
 
 -- !!!!!!!!!!!!!!! VOIR SI AUTORISEE SINON RECODER A LA MAIN !!!!!!!!!!!!!!!
+-- Val a dit autorisé
 import Data.List (elemIndex)
 -- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
 
 -- | LZW compress method
 compress :: String -> [Int]
@@ -25,19 +25,36 @@ compress_method :: String -> Int -> [Int] -> [String] -> [Int]
 compress_method [] _ list_int _ = list_int
   
 -- Condition d'arrêt length(message) = n    <=>  mon dernier patern est un patern que je connais.
-compress_method message (length message) list_int dictionary = 
-  list_int ++ [(elemIndex (take (length message) message) dictionary)]
-  where dictionary = dictionary ++ [message]
+compress_method message length_message list_int dictionary = 
+  list_int ++ index_patern
+  where 
+    updated_dictionary = dictionary ++ [message]
+    length_message = length message
+    patern = take length_message message
+    maybeIndex = elemIndex patern updated_dictionary
+    index_patern = case maybeIndex of
+      Just index -> [index]
+      Nothing -> []
+
+
 
 -- Tous les autres cas
-compress_method message n list_int dictionary =
+compress_method message n list_int dictionary
   -- Soit message dans liste et n+1 < length(message)
   -- Si le patern crée avec les n premiers caractères de message est dans le dictionnaire on prend un caractère de plus
   | (take n message) `elem` dictionary = compress_method message (n+1) list_int dictionary
   -- Si le patern n'est pas dans la liste
-  | otherwise = compress_method (drop (n-1) message) 2 (list_int ++ [(elemIndex (take n message) dictionary)]) dictionary
-  -- !!!!!!!!!!!!!!!!! VERIFIER QUE CE WHERE S APPLIQUE QU A CE | !!!!!!!!!!!!!!!
-  where dictionary = dictionary ++ [(take n message)]
+  | otherwise = compress_method new_message 2 new_list_int updated_dictionary
+  -- Le where s'applique qu'à mon otherwise
+  where 
+    updated_dictionary = dictionary ++ [(take n message)]
+    new_message = drop (n-1) message
+    patern = take n message
+    maybeIndex = elemIndex patern updated_dictionary
+    index_patern = case maybeIndex of
+      Just index -> [index]
+      Nothing -> []
+    new_list_int = list_int ++ [maybeIndex]
 
 
 -- | LZW uncompress method
