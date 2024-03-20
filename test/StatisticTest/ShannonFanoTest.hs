d45e7b46c1ba8a989398d1eb106b5618692f4fa8
RolletQuentin <rolletquen@gmail.com>
2024-03-20 18:03:03 +0100

    update Shannon-Fano tests

--- test/StatisticTest/ShannonFanoTest.hs
+++ test/StatisticTest/ShannonFanoTest.hs
@@ -18,28 +18,22 @@ testShannonFano = TestList
         "Empty input" ~: buildEncodingTree "" ~?= Nothing,
         "Single character input" ~: buildEncodingTree "a" ~?= Just (EncodingLeaf 1 'a'),
         "Multiple characters with different frequencies" ~:
-            buildEncodingTree "abcdd" ~?= Just (EncodingNode 5
-                (EncodingLeaf 1 'a')
-                (EncodingNode 4
-                    (EncodingLeaf 1 'b')
-                    (EncodingNode 3
-                        (EncodingNode 2
-                            (EncodingLeaf 1 'd')
-                            (EncodingLeaf 1 'c')
-                        )
-                        (EncodingLeaf 1 'd')
-                    )
+            buildEncodingTree "abbca" ~?= Just (EncodingNode 5
+                (EncodingLeaf 2 'b')
+                (EncodingNode 3
+                    (EncodingLeaf 2 'a')
+                    (EncodingLeaf 1 'c')
                 )
             ),
         "Multiple characters with equal frequencies" ~:
             buildEncodingTree "abcd" ~?= Just (EncodingNode 4
                 (EncodingNode 2
-                    (EncodingLeaf 1 'b')
-                    (EncodingLeaf 1 'a')
-                )
-                (EncodingNode 2
                     (EncodingLeaf 1 'd')
                     (EncodingLeaf 1 'c')
+                )
+                (EncodingNode 2
+                    (EncodingLeaf 1 'b')
+                    (EncodingLeaf 1 'a')
                 )
             )
     ]
